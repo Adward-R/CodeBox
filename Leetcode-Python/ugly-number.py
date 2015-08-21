283745e0cab53f75377b8df92cc4e8715bbd2936
Eddie <adward_r@icloud.com>
2015-08-21 12:31:55 +0800

    Leetcode:ugly-numbers

---
+++ Leetcode-Python/ugly-number.py
@@ -0,0 +1,23 @@
+__author__ = 'Adward'
+
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        while num % 2 == 0:
+            num /= 2
+        while num % 3 == 0:
+            num /= 3
+        while num % 5 == 0:
+            num /= 5
+        if num == 1:
+            return True
+        else:
+            return False
+
+sol = Solution()
+print(sol.isUgly(0))
