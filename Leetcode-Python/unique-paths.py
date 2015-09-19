83470f9c514dd386b4e88a9077fe19324b9d8e22
Eddie <adward_r@icloud.com>
2015-09-19 17:00:31 +0800

    Leetcode: unique-paths-ii

--- Leetcode-Python/unique-paths.py
+++ Leetcode-Python/unique-paths.py
@@ -21,5 +21,46 @@ class Solution(object):
 
         return matrix[-1][-1]
 
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m = len(obstacleGrid)
+        if m <= 0:
+            return 0
+        n = len((obstacleGrid[0]))
+        if n <= 0:
+            return 0
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        matrix = []
+        blocked = False
+        for i in range(m):
+            if blocked:
+                matrix.append([0 for k in range(n)])
+            else:
+                if obstacleGrid[i][0] == 1:
+                    matrix.append([0 for k in range(n)])
+                    blocked = True
+                else:
+                    matrix.append([1] + [0 for k in range(n-1)])
+        #blocked = False
+        for j in range(1, n):
+            if obstacleGrid[0][j] == 1:
+                break
+            else:
+                matrix[0][j] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    matrix[i][j] = 0
+                else:
+                    matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+
+        return matrix[-1][-1]
+
 sol = Solution()
-print(sol.uniquePaths(3,3))
+print(sol.uniquePathsWithObstacles([[0,1,0]]))
