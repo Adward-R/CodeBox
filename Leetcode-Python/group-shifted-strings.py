12d0baee64197f1d8d878658eb775ff1d2d30e03
Eddie <adward_r@icloud.com>
2016-06-24 16:19:23 +0800

    Leetcode: 249.grouped-shifted-strings

---
+++ Leetcode-Python/group-shifted-strings.py
@@ -0,0 +1,28 @@
+__author__ = 'Adward'
+
+
+class Solution(object):
+    def groupStrings(self, strings):
+        """
+        :type strings: List[str]
+        :rtype: List[List[str]]
+        """
+        groups = {}
+        for s in strings:
+            shamt = ord('a') - ord(s[0])
+            if shamt != 0:
+                key = ''.join([
+                    chr(ord('a') + (ord(ch) - ord('a') + shamt) % 26)
+                    for ch in s
+                ])
+            else:
+                key = s
+            if key in groups:
+                groups[key].append(s)
+            else:
+                groups[key] = [s]
+        return [groups[key] for key in groups]
+
+sol = Solution()
+strings = ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"]
+print(sol.groupStrings(strings))
