681d8be3c4ca12c9346e0e617cbd9f7482261eda
Eddie <adward_r@icloud.com>
2016-07-19 14:52:25 +0800

    Leetcode: flip-game-ii

--- Leetcode-Python/flip-game.py
+++ Leetcode-Python/flip-game.py
@@ -1,46 +1,61 @@
 __author__ = 'Adward'
 class Solution(object):
+    def __init__(self):
+        self.dict = {
+            '+': True,
+            '++': False,
+            '+++': False,
+            '++++': False,
+            '+++++': True,
+            '++++++': False
+        }
+
     def generatePossibleNextMoves(self, s):
         """
         :type s: str
         :rtype: List[str]
         """
-        nextStrs = []
+        nextStrs = set()
         for i in range(len(s)-1):
             if s[i] == '+' and s[i+1] == '+':
-                nextStrs.append(''.join([s[0:i], '--', s[i+2:]]))
+                if 1 < i < len(s) - 3:
+                    nextStrs.add(''.join([s[0:i], '--', s[i+2:]]))
+                elif i <= 1:
+                    nextStrs.add(s[i+2:].lstrip('-'))
+                else:
+                    nextStrs.add(s[:i].rstrip('-'))
         return nextStrs
+
+    def roundPlay(self, s):  # opponent faces s
+        if s in self.dict:
+            return self.dict[s]
+        strs = self.generatePossibleNextMoves(s)
+        for ss in strs:  # self faces ss
+            nexts = self.generatePossibleNextMoves(ss)
+            if len(nexts) == 0:
+                self.dict[s] = False
+                return False
+            for sss in nexts:
+                if self.roundPlay(sss):
+                    break
+            else:
+                self.dict[s] = False
+                return False
+        self.dict[s] = True
+        return True
 
     def canWin(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        firstSteps = self.generatePossibleNextMoves(s)
-        for fs in firstSteps:  # opponents faces 'fs'
-            try:
-                q = self.generatePossibleNextMoves(fs)
-                curntLeng = len(q)
-                while curntLeng:
-                    for k in range(curntLeng):
-                        # self faces q[k]
-                        strs = self.generatePossibleNextMoves(q[k])
-                        if len(strs) == 0:
-                            raise
-                        for ss in strs:  # opponent faces ss
-                            rtstrs = self.generatePossibleNextMoves(ss)
-                            if len(rtstrs):
-                                q += rtstrs
-                    q = q[curntLeng:]
-                    curntLeng = len(q)
+        for fs in self.generatePossibleNextMoves(s):
+            if self.roundPlay(fs):
                 return True
-            except:
-                pass
         return False
 
 
-
-
 sol = Solution()
-# print(sol.generatePossibleNextMoves('++++'))
-print(sol.canWin('++++-++++++'))
+# print(sol.generatePossibleNextMoves('+++++++++++++++++++++'))
+print(sol.canWin("+++++++++++++++++++++"))
+# print(sol.roundPlay(''))
