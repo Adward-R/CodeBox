2247c00ad40631c81f0f13cf86dff133c6dea97f
Eddie <adward_r@icloud.com>
2017-01-10 15:10:25 -0500

    lc364 nested-list-weight-sum-ii

--- Leetcode-Python/nested-list-weight-sum.py
+++ Leetcode-Python/nested-list-weight-sum.py
@@ -1,4 +1,6 @@
 __author__ = 'Adward'
+# including -i & -ii
+
 # """
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
@@ -35,3 +37,23 @@ class Solution(object):
             return sum([curDepth * nInt.getInteger() if nInt.isInteger() else helper(nInt.getList(), curDepth+1)
                         for nInt in nList])
         return helper(nestedList, 1)
+
+    def depthSumInverse(self, nestedList):
+        """
+        :type nestedList: List[NestedInteger]
+        :rtype: int
+        """
+        D = {}
+
+        def getDepth(nList, curDepth):
+            if curDepth not in D:
+                D[curDepth] = []
+            for ni in nList:
+                if ni.isInteger():
+                    D[curDepth].append(ni.getInteger())
+                else:
+                    getDepth(ni.getList(), curDepth+1)
+
+        getDepth(nestedList, 1)
+        mx = max(D)
+        return sum([sum(D[depth]) * (mx + 1 - depth) for depth in D])
