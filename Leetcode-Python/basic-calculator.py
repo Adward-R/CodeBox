6cf764118ab44adf3b61cd3ed3ae3b7413fd5142
Eddie <adward_r@icloud.com>
2017-02-13 14:42:01 -0500

    lc224 basic-calculator

--- Leetcode-Python/basic-calculator.py
+++ Leetcode-Python/basic-calculator.py
@@ -1,6 +1,51 @@
 __author__ = 'Adward'
 class Solution(object):
     def calculate(self, s):
+        stk = []
+        num, sign, result = 0, 1, 0
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == '+' or ch == '-':
+                result += sign * num
+                num = 0
+                sign = 1 if ch == '+' else -1
+            elif ch == '(':
+                stk += [result, sign]
+                result, sign = 0, 1
+            elif ch == ')':
+                result += sign * num
+                num = 0
+                result *= stk.pop()  # set the sign of result in current ()
+                result += stk.pop()  # before the sign is the last result calculated in outer scope
+
+        if num:
+            result += sign * num
+        return result
+
+    def calculate1(self, s):
+        seq = ''
+        balance = 0
+        i = 0
+        while i < len(s):
+            if s[i] == '-':
+                seq += '+' if balance % 2 else '-'
+                if i + 1 < len(s) and s[i + 1] == '(':
+                    balance += 1
+            elif s[i] == ')' and balance > 0:
+                balance -= 1
+            elif s[i] == '+':
+                seq += '-' if balance % 2 else '+'
+            elif s[i].isdigit():
+                seq += s[i]
+            i += 1
+        ret = 0
+        for subseq in seq.split('+'):
+            sublist = [int(d) for d in subseq.split('-')]
+            ret += sublist[0] - sum(sublist[1:])
+        return ret
+
+    def calculate0(self, s):
         """
         :type s: str
         :rtype: int
@@ -36,5 +81,5 @@ class Solution(object):
         return ssum[0]
 
 sol = Solution()
-#print(sol.calculate('(1+(4+5+2)-3)-(6+8)'))
-print(sol.calculate('2-(5-6)'))
+print(sol.calculate('(1+(4+5+2)-3)-(6+8)'))
+print(sol.calculate('1-(2-(3-4)-5)'))
