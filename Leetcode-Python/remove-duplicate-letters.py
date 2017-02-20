89df5972e7e015a17eb3c78c64557a02a1ca00ad
Eddie <adward_r@icloud.com>
2017-02-19 21:07:19 -0500

    lc316 remove-duplicate-letters

--- Leetcode-Python/remove-duplicate-letters.py
+++ Leetcode-Python/remove-duplicate-letters.py
@@ -1,6 +1,16 @@
 __author__ = 'Adward'
 from collections import Counter
 class Solution(object):
+    def removeDuplicateLetters_fast(self, s):
+        rindex = {c: i for i, c in enumerate(s)}  # last appearance
+        result = []
+        for i, c in enumerate(s):
+            if c not in result:
+                while result and c < result[-1] and i < rindex[result[-1]]:
+                    result.pop()
+                result.append(c)
+        return ''.join(result)
+
     def removeDuplicateLetters(self, s):
         """
         :type s: str
@@ -16,62 +26,7 @@ class Solution(object):
                 break
         return s[pos] + self.removeDuplicateLetters(s[pos+1:].replace(s[pos], "")) if len(s) else ""
 
-
-
-    def removeDuplicateLetters0(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        BASE_A = ord('a')
-        N_LETTERS = 26
-        alphabet = []
-        used = [False] * N_LETTERS
-
-        for i in range(N_LETTERS):
-            alphabet.append([])
-        for i in range(len(s)):
-            alphabet[ord(s[i]) - BASE_A].append(i)
-
-        totalLeng = 0
-        #smallestRightMost = len(s) - 1
-        for i in range(N_LETTERS):
-            if len(alphabet[i]):
-                pass#totalLeng += 1
-            else:
-                used[i] = True
-                #tmp = alphabet[i][-1]
-                #if tmp < smallestRightMost:
-                    #smallestRightMost = tmp
-
-        rightMosts = []
-        for lst in alphabet:
-            if len(lst):
-                rightMosts.append(lst[-1])
-        #rightMosts.sort(reverse=True)
-
-        seq = ''
-        curRightMost = -1
-
-        for i in range(N_LETTERS):
-            flag = False
-            for j in range(N_LETTERS):
-                if not used[j] and alphabet[j][-1] > curRightMost:
-                    for idx in alphabet[j]:
-                        mmin = min(rightMosts)
-                        if curRightMost < idx <= mmin: #- totalLeng + curLeng + 1:
-                            curRightMost = idx
-                            used[j] = True
-                            seq += chr(BASE_A + j)
-                            #if alphabet[j][-1] == rightMosts[-1]:
-                            #    rightMosts.pop()
-                            del rightMosts[rightMosts.index(alphabet[j][-1])]
-                            flag = True
-                            break
-                if flag:
-                    break
-        return seq
-
 sol = Solution()
 #print(sol.removeDuplicateLetters('cbacdcbc'))
-print(sol.removeDuplicateLetters("caccabad"))
+# print(sol.removeDuplicateLetters_fast("caccabad"))
+print(sol.removeDuplicateLetters_fast("bcabc"))
